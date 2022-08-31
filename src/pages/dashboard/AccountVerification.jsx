3084b3b0fb656dc8ddf10be92776cadbe72e2a53
Babatunde-Ben <babatundeben110@gmail.com>
2022-09-01 00:56:38 +0100

    style: modified account verification screen

--- src/pages/dashboard/AccountVerification.jsx
+++ src/pages/dashboard/AccountVerification.jsx
@@ -13,6 +13,8 @@ import "../../styles/registration.css";
 
 const AccountVerification = () => {
   const [isOpen, setIsOpen] = useState(false);
+  const [phoneVerificationCode, setPhoneVerificationCode] = useState("");
+  const [emailVerificationCode, setEmailVerificationCode] = useState("");
   return (
     <div className="w-screen">
       <div className="w-full flex flex-row justify-start items-start">
@@ -32,50 +34,45 @@ const AccountVerification = () => {
               />
             </div>
             {!isOpen && (
-              <>
+              <div className="p-5 md:ml-16 lg:ml-20">
                 <h1 className="font-semibold text-3xl text-left text-gray-900 mb-6 sm:text-center  lg:text-left">
                   Account Verification
                 </h1>
-                <h3 className="text-left mb-16 sm:text-center  lg:text-left md:w-[400px] md:mx-auto lg:mx-0">
+                <h3 className="text-left mb-8 sm:text-center  lg:text-left md:w-[400px] md:mx-auto lg:mx-0">
                   Verification codes have been sent to your email address and
                   phone number. Enter them in the respective boxes below.
                 </h3>
                 <form>
                   <label className="mb-5 block">
+                    <p className="registration-input-label ">Phone Number</p>
                     <div className="relative sm:w-[400px] sm:mx-auto lg:mx-0">
                       <input
                         type="text"
-                        className={`registration-input `}
-                        name="email"
+                        className={`registration-input`}
+                        name="tel"
                         placeholder="Enter code here"
-                        // value={verificationCode}
-                        // onChange={(e) => {
-                        //   setVerificationCode(e.target.value);
-                        // }}
+                        value={phoneVerificationCode}
+                        onChange={(e) => {
+                          setPhoneVerificationCode(e.target.value);
+                        }}
                       />
                     </div>
                   </label>
                   <div className="my-8">
                     <PrimaryButton
-                    //   handle={(e) => {
-                    //     e.preventDefault();
-                    //     if (verificationCode.length > 0) {
-                    //       // setIsVerified(true);
-                    //       console.log("Verified");
-                    //       navigate(`/create-new-password/${verificationCode}`, {
-                    //         replace: true,
-                    //       });
-                    //     }
-                    //   }}
+                      handle={(e) => {
+                        e.preventDefault();
+                        console.log("verify phone number");
+                      }}
                     >
-                      Create New Password
+                      Verify Phone Number
                     </PrimaryButton>
                   </div>
                 </form>
                 <div className="mb-6 lg:w-[450px]">
                   <div className="lg:flex lg:items-center lg:gap-3">
                     <p className="text-black font-medium mt-2 mb-2">
-                      Didn't receive any email?
+                      Didn't receive any code?
                     </p>
                     <p
                       className="text-secondary font-medium cursor-pointer"
@@ -84,26 +81,58 @@ const AccountVerification = () => {
                       //     setEmail("");
                       //   }}
                     >
-                      Try another email address
-                    </p>
-                  </div>
-                  <div className="lg:flex lg:items-center lg:justify-center lg:gap-3 ">
-                    <p className="text-black font-medium mt-2 mb-2">Or</p>
-                    <p
-                      //   onClick={() => {
-                      //     if (email) {
-                      //       requestPasswordReset();
-                      //     } else {
-                      //       setIsResetEmail("");
-                      //     }
-                      //   }}
-                      className="text-secondary font-medium cursor-pointer"
-                    >
-                      Resend Instructions
+                      Resend verification code
                     </p>
                   </div>
                 </div>
-              </>
+
+                {/* verify user email address */}
+                <form className="mt-10">
+                  <label className="mb-5 block">
+                    <p className="registration-input-label">
+                      Email Address (Optional)
+                    </p>
+                    <div className="relative sm:w-[400px] sm:mx-auto lg:mx-0">
+                      <input
+                        type="text"
+                        className={`registration-input`}
+                        name="email"
+                        placeholder="Enter code here"
+                        value={emailVerificationCode}
+                        onChange={(e) => {
+                          setEmailVerificationCode(e.target.value);
+                        }}
+                      />
+                    </div>
+                  </label>
+                  <div className="my-8">
+                    <PrimaryButton
+                      handle={(e) => {
+                        e.preventDefault();
+                        console.log("verify phone number");
+                      }}
+                    >
+                      Verify Email
+                    </PrimaryButton>
+                  </div>
+                </form>
+                <div className="mb-6 lg:w-[450px]">
+                  <div className="lg:flex lg:items-center lg:gap-3">
+                    <p className="text-black font-medium mt-2 mb-2">
+                      Didn't receive any code?
+                    </p>
+                    <p
+                      className="text-secondary font-medium cursor-pointer"
+                      //   onClick={() => {
+                      //     setIsResetEmail("");
+                      //     setEmail("");
+                      //   }}
+                    >
+                      Resend verification code
+                    </p>
+                  </div>
+                </div>
+              </div>
             )}
           </Container>
         </div>
