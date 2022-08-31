c81890dc6887ea10473f0b0bc8aa31e0d93289a2
Babatunde-Ben <babatundeben110@gmail.com>
2022-08-31 23:25:07 +0100

    fix: modified forgot password text

---
+++ src/pages/dashboard/AccountVerification.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from "react";
+// import { NavLink, Outlet } from "react-router-dom";
+
+// import components
+import Container from "../../components/container/Container";
+import Header from "../../components/head/Header";
+import MobileHeader from "../../components/head/MobileHeader";
+import SideMenu from "../../components/nav/SideBar";
+
+// import styles
+// import "../../"
+
+const AccountVerification = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  // const [activeTab, setActiveTab] = useState("tab2");
+  return (
+    <div className="w-screen">
+      <div className="w-full flex flex-row justify-start items-start">
+        <div className="w-1/5 hidden lg:flex md:flex h-screen fixed mr-auto">
+          <SideMenu selectHistory={true} />
+        </div>
+        <div className="lg:w-4/5 w-full ml-auto">
+          <Container>
+            <div className="w-full my-5 lg:flex md:flex hidden">
+              <Header />
+            </div>
+            <div className="w-screen lg:hidden md:hidden flex">
+              <MobileHeader
+                selectHistory={true}
+                open={isOpen}
+                setOpen={() => setIsOpen(!isOpen)}
+              />
+            </div>
+            {!isOpen && (
+              <>
+                <h1 className="font-semibold text-3xl text-left text-gray-900 mb-6 sm:text-center  lg:text-left">
+                  Code Verification
+                </h1>
+                <h3 className="text-left mb-16 sm:text-center  lg:text-left md:w-[400px] md:mx-auto lg:mx-0">
+                  {/* We have sent password recovery instructions to your mail */}
+                  We have sent password recovery instructions to your mail and
+                  phone number
+                </h3>
+                <form>
+                  <label className="mb-5 block">
+                    <div className="relative sm:w-[400px] sm:mx-auto lg:mx-0">
+                      <input
+                        type="text"
+                        className={`registration-input ${
+                          isEmailValid === "invalid" && "invalid"
+                        } ${isEmailValid === "valid" && "valid"}`}
+                        name="email"
+                        placeholder="Enter the code here"
+                        value={verificationCode}
+                        onChange={(e) => {
+                          setVerificationCode(e.target.value);
+                        }}
+                      />
+
+                      {/* <img
+                      src={AlertIcon}
+                      alt=""
+                      className={`registration-input-icon  ${
+                        isEmailValid === "invalid" ? "visible" : "hidden"
+                      }`}
+                    />
+                    <img
+                      src={CheckIcon}
+                      alt=""
+                      className={`registration-input-icon  ${
+                        isEmailValid === "valid" ? "visible" : "hidden"
+                      }`}
+                    /> */}
+                    </div>
+                  </label>
+                  <div className="my-8">
+                    <PrimaryButton
+                      handle={(e) => {
+                        e.preventDefault();
+                        if (verificationCode.length > 0) {
+                          // setIsVerified(true);
+                          console.log("Verified");
+                          navigate(`/create-new-password/${verificationCode}`, {
+                            replace: true,
+                          });
+                        }
+                      }}
+                    >
+                      Create New Password
+                    </PrimaryButton>
+                  </div>
+                </form>
+                <div className="mb-6 lg:w-[450px]">
+                  <div className="lg:flex lg:items-center lg:gap-3">
+                    <p className="text-black font-medium mt-2 mb-2">
+                      Didn't receive any email?
+                    </p>
+                    <p
+                      className="text-secondary font-medium cursor-pointer"
+                      onClick={() => {
+                        setIsResetEmail("");
+                        setEmail("");
+                      }}
+                    >
+                      Try another email address
+                    </p>
+                  </div>
+                  <div className="lg:flex lg:items-center lg:justify-center lg:gap-3 ">
+                    <p className="text-black font-medium mt-2 mb-2">Or</p>
+                    <p
+                      onClick={() => {
+                        if (email) {
+                          requestPasswordReset();
+                        } else {
+                          setIsResetEmail("");
+                        }
+                      }}
+                      className="text-secondary font-medium cursor-pointer"
+                    >
+                      Resend Instructions
+                    </p>
+                  </div>
+                </div>
+              </>
+            )}
+          </Container>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AccountVerification;
