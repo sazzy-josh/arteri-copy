56671dc4a8e7383a85e04ae3636bf338707a06a8
Babatunde-Ben <babatundeben110@gmail.com>
2022-10-27 00:34:19 +0100

    modified provider account

--- src/pages/provider-dashboard/MyAccount.js
+++ src/pages/provider-dashboard/MyAccount.js
@@ -27,61 +27,6 @@ const ProviderAccount = () => {
             <p className="text-2xl font-bold">My Account</p>
           </div>
         </div>
-        {/* <div className="w-full">
-          <div className="w-full">
-            <div className="flex flex-row justify-start items-start whitespace-nowrap w-2/3">
-              <p
-                onClick={() => {
-                  setProfile(true);
-                  setPassword(false);
-                  setSecurity(false);
-                }}
-                className={
-                  showProfile
-                    ? `w-full text-left pl-5 font-semibold cursor-pointer border-b-2 border-b-orange-500 pb-5`
-                    : `w-full text-left pl-5 font-semibold cursor-pointer pb-5`
-                }
-              >
-                Profile
-              </p>
-              <p
-                onClick={() => {
-                  setProfile(false);
-                  setPassword(true);
-                  setSecurity(false);
-                }}
-                className={
-                  showPassword
-                    ? `w-full text-left pl-5 font-semibold cursor-pointer border-b-2 border-b-orange-500 pb-5`
-                    : `w-full text-left pl-5 font-semibold cursor-pointer pb-5`
-                }
-              >
-                Password Settings
-              </p>
-              <p
-                onClick={() => {
-                  setProfile(false);
-                  setPassword(false);
-                  setSecurity(true);
-                }}
-                className={
-                  showSecurity
-                    ? `w-full text-left pl-5 font-semibold cursor-pointer border-b-2 border-b-orange-500 pb-5`
-                    : `w-full text-left pl-5 font-semibold cursor-pointer pb-5`
-                }
-              >
-                Security Settings
-              </p>
-            </div>
-            <hr className="w-full bg-gray-400 lg:mx-0 mx-5" />
-          </div>
-        </div> */}
-
-        {/* <div className="w-full lg:px-0 px-5">
-          {showProfile && <Profile />}
-          {showPassword && <Password />}
-          {showSecurity && <Security />}
-        </div> */}
 
         <Tabs
           tabsArray={[
@@ -93,9 +38,6 @@ const ProviderAccount = () => {
 
         <div className="w-full lg:px-0 px-5">
           <Outlet />
-          {/* {showProfile && <Profile />}
-          {showPassword && <Password />}
-          {showSecurity && <Security />} */}
         </div>
       </div>
     </ProviderDashboardWrapper>
